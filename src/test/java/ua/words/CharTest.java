65589e8f26539229694070dab68eaf6f34be12ee
vnsmn <01_kilimandjaro@ukr.net>
2015-02-06 15:52:35 +0200

    --

---
+++ src/test/java/ua/words/CharTest.java
@@ -0,0 +1,11 @@
+package ua.words;
+
+import org.junit.Test;
+
+public class CharTest {
+
+    @Test
+    public void test() {
+        System.out.print(Integer.toHexString(Character.codePointAt(" ", 0)));
+    }
+}
